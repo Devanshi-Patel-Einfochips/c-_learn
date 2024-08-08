b3c1f7e482b1eeb4d0bdb77a08e864d8681ef4c8
devanshi <devanshi.patel1@einfochips.com>
2024-08-08 06:20:25 +0000

    first

---
+++ bankingmain.cpp
@@ -0,0 +1,22 @@
+#include<string>
+#include<iostream>
+#include "banking.cpp"
+#include "banking.h"
+#include "Checking.h"
+#include "Checking.cpp"
+#include "Transact.cpp"
+#include "savings.cpp"
+#include "savings.h"
+using namespace std;
+
+int main(){
+    // checking acc("dev",10000);
+    // cout << "Initial balance: " << acc.getBalance() << endl;
+    // // acc.Deposit(500);
+    // acc.withdraw(9980);
+    // cout << "New balance: " << acc.getBalance() << endl;
+    // Savings acc("devs", 10000, 0.05f);
+    Savings *acc = new Savings("devs", 10000, 0.05f);
+    // transact_fun(acc);
+    delete acc;
+};
